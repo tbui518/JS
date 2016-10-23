8b924e2c6af90bf9a1f53fe04c2b1bcb42d2682a
Jay <tbui518@gmail.com>
2016-10-23 00:12:08 -0400

    updates

---
+++ object.js
@@ -0,0 +1,19 @@
+//var div = document.getElementById("test");
+
+//div.innerHTML = "this is new text";
+
+var person = {
+
+    name: "Jay",
+    surname: "Bui",
+    toString: function ()
+   {
+    return name + " " + surname;
+   }
+};
+
+//person.name = "Tri";
+console.log(person.toString);
+
+//div.innerHTML = person;
+
