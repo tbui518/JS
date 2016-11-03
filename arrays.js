c8e3c26bebadcc60a10f5dc419b885ab87cfb666
tbui518 <tbui518@gmail.com>
2016-11-03 10:57:17 -0400

    update

--- arrays.js
+++ arrays.js
@@ -6,5 +6,17 @@ var products = [
 ];
 
 products[4] = "nodejs";
+products[products.length-3] = "c++";
+products.push("reactjs");
 
+console.log(products);
 console.log(products[3]);
+console.log(products[4]);
+console.log(products[5]);
+
+var person = [];
+
+person["name"] = "Jay";
+person["surname"] = "Bui";
+
+console.log(person.name);
