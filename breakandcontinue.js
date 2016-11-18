826cb30bdbf8de7bc2fe3b23b3729bb2d9aa8671
Jay <tbui518@gmail.com>
2016-11-18 00:10:30 -0500

    update

---
+++ breakandcontinue.js
@@ -0,0 +1,17 @@
+/*for ( var i = 0; i < 5; i++)
+{
+    if (i === 2)
+    break;
+
+    console.log(i);
+
+}
+*/
+for ( var i = 0; i < 5; i++)
+{
+    if (i === 2)
+    continue;
+
+    console.log(i);
+    
+}
---
+++ forloop.js
@@ -0,0 +1,11 @@
+var videoCourses = [
+    "PHP",
+    "Mysql",
+
+];
+
+for(var i = 0; i < 5; i++)
+{
+    document.getElementById("result").innerHTML += i + "<br>";
+    i++;
+}
